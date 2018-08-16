2e228dfafe8f266cfd7e932db081504d0070cc39
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-08-16 18:05:23 +0900

    meshを使ってグラデーションカラーの作成

---
+++ meshColor/src/ofApp.cpp
@@ -0,0 +1,138 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofBackground(0);
+    mesh.setMode(OF_PRIMITIVE_TRIANGLE_FAN);
+    corners[0] = ofVec3f(0, 0, 0);
+    corners[1] = ofVec3f(0, ofGetHeight(), 0);
+    corners[2] = ofVec3f(ofGetWidth(), 0, 0);
+    corners[3] = ofVec3f(ofGetWidth(), ofGetHeight(), 0);
+    int target = 0;
+    for (int i = 0; i < meshSize; i++) {
+        if (i % (meshSize / 4) == 0) {
+            vertices.push_back(corners[target]);
+            colors.push_back(ofFloatColor::fromHsb(ofRandom(0.3, 0.8), 1.0, 1.0));
+            target++;
+        }
+        vertices.push_back(ofVec3f(ofRandomWidth(), ofRandomHeight(), ofRandom(-ofGetWidth() / 2, ofGetWidth() / 2)));
+        colors.push_back(ofFloatColor::fromHsb(ofRandom(0, 1.0), ofRandom(0.8, 1.0), ofRandom(0.8, 1.0)));
+    }
+    for (int i = 0; i < vertices.size(); i++) {
+        velocity.push_back(ofVec3f(ofRandom(-2.0, 2.0), ofRandom(-2.0, 2.0), 0.0));
+    }
+    for (int i = 0; i < vertices.size(); i++) {
+        middle += vertices[i];
+    }
+    middle /= float(vertices.size());
+    cam.lookAt(middle);
+    orbit = cam.getPosition();
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    mesh.clear();
+    for (int i = 0; i < vertices.size(); i++) {
+        vertices[i] += velocity[i];
+        if (vertices[i].x < 0 || vertices[i].x > ofGetWidth()) {
+            velocity[i].x *= -1.0;
+        }
+        if (vertices[i].y < 0 || vertices[i].y > ofGetHeight()) {
+            velocity[i].y *= -1;
+        }
+    }
+    mesh.addVertices(vertices);
+    mesh.addColors(colors);
+    orbit.x *= 200.0 * sin(ofGetElapsedTimef());
+    orbit.y *= 200.0 * cos(ofGetElapsedTimef());
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    easyCam.begin();
+    mesh.draw();
+    ofBoxPrimitive box = ofBoxPrimitive(100, 100, 100);
+    box.setPosition(orbit);
+    box.draw();
+    easyCam.end();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    switch (key) {
+        case '0':
+            mesh.setMode(OF_PRIMITIVE_TRIANGLE_FAN);
+            ofSetWindowTitle("OF_PRIMITIVE_TRIANGLE_FAN");
+            break;
+        case '1':
+            mesh.setMode(OF_PRIMITIVE_TRIANGLE_STRIP);
+            ofSetWindowTitle("OF_PRIMITIVE_TRIANGLE_STRIP");
+            break;
+        case '2':
+            mesh.setMode(OF_PRIMITIVE_TRIANGLES_ADJACENCY);
+            ofSetWindowTitle("OF_PRIMITIVE_TRIANGLES_ADJACENCY");
+            break;
+        case '3':
+            mesh.setMode(OF_PRIMITIVE_LINE_LOOP);
+            ofSetWindowTitle("OF_PRIMITIVE_LINE_LOOP");
+            break;
+        case '4':
+            mesh.setMode(OF_PRIMITIVE_TRIANGLES);
+            ofSetWindowTitle("OF_PRIMITIVE_TRIANGLES");
+            break;
+        case '5':
+            mesh.setMode(OF_PRIMITIVE_LINE_STRIP_ADJACENCY);
+            ofSetWindowTitle("OF_PRIMITIVE_LINE_STRIP_ADJACENCY");
+            break;
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ meshColor/src/ofApp.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+    ofMesh mesh;
+    int meshSize = 200;
+    vector<ofVec3f> vertices;
+    vector<ofFloatColor> colors;
+    vector<ofVec3f> velocity;
+    ofVec3f corners[4];
+    ofCamera cam;
+    ofEasyCam easyCam;
+    ofVec3f orbit;
+    ofVec3f middle;
+    void printCamPosition(ofEasyCam cam) {
+        cout << cam.getPosition() << endl;
+    }
+};
