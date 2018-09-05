bc9916f145cc92b5667b27f55af42edd69386ee7
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-09-05 20:44:04 +0900

    ImageHackの軽微な修正

--- ImageHack/src/ofApp.cpp
+++ ImageHack/src/ofApp.cpp
@@ -14,9 +14,9 @@ void ofApp::setup(){
             float green = ofMap(image.getColor(w, h).g, 0, 255, 0, 1.0);
             float blue = ofMap(image.getColor(w, h).b, 0, 255, 0, 1.0);
             colors[h + w * HEIGHT] = ofFloatColor(red, green, blue);
-            points[h + w * HEIGHT] = ofVec3f(ofRandomWidth(), ofRandomHeight(), ofRandom(-1000));
+            points[h + w * HEIGHT] = ofVec3f(w, h, 0);
             temp[h + w * HEIGHT] = ofVec3f(w, h, 0);
-            vel[h + w * HEIGHT] = ofVec3f(ofRandom(-10, 10), ofRandom(-10, 10), ofRandom(-5, 5));
+            vel[h + w * HEIGHT] = ofVec3f(ofRandom(-10, 10), ofRandom(-10, 10), 0);
         }
     }
     myVbo.setVertexData(points, WIDTH * HEIGHT, GL_DYNAMIC_DRAW);
@@ -50,8 +50,14 @@ void ofApp::draw(){
     if (mode == -1) {
 //        image.draw(0, 0);
     } else {
-        glPointSize(1);
-        myVbo.draw(GL_POINTS, 0, WIDTH * HEIGHT);
+//        glPointSize(1);
+//        myVbo.draw(GL_POINTS, 0, WIDTH * HEIGHT);
+        for (int w = 0; w < WIDTH; w++) {
+            for (int h = 0; h < HEIGHT; h++) {
+                ofSetColor(colors[h + w * HEIGHT]);
+                ofDrawRectangle(points[h + w * HEIGHT].x, points[h + w * HEIGHT].y, 1, 1);
+            }
+        }
     }
 }
 
