e12e69383cc8a668b6a8c8471e40544fbe42efb3
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-09-16 23:31:02 +0900

    sinを用いたプロジェクト新規作成

---
+++ lighringParticle/src/LightingParticle.cpp
@@ -0,0 +1,8 @@
+//
+//  LightingParticle.cpp
+//  lighringParticle
+//
+//  Created by 斉藤俊介 on 2018/09/14.
+//
+
+#include "LightingParticle.hpp"
---
+++ lighringParticle/src/LightingParticle.hpp
@@ -0,0 +1,13 @@
+//
+//  LightingParticle.hpp
+//  lighringParticle
+//
+//  Created by 斉藤俊介 on 2018/09/14.
+//
+
+#ifndef LightingParticle_hpp
+#define LightingParticle_hpp
+
+#include <stdio.h>
+
+#endif /* LightingParticle_hpp */
